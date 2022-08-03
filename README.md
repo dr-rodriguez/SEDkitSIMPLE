e32bfabaa6c5e480091c60d1f4445cd72d2d77d3
David Rodriguez <drodriguez@stsci.edu>
2022-08-03 16:45:29 -0400

    updating readme and initial commit of SEDSIMPLE Class

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# SEDkitSIMPLE
+# SEDkit with SIMPLE
+
+To prepare, make sure your environment has the latest version of SEDkit and AstrodbKit2:
+
+```bash
+pip install sedkit
+pip install AstrodbKit2
+```
+
+You will also need to have a copy of the SIMPLE database. 
+If you have it in your current working directory you can connect to it with:
+
+```python
+from astrodbkit2.astrodb import Database
+db = Database('sqlite:///SIMPLE.db')
+```
+
+This will need to be passed to `SEDSIMPLE` for it to work.
+
+The key functionality in this package is a new class (`SEDSIMPLE`) that inherits from `SED` in order 
+to provide wrappers around SIMPLE Database calls. 
+For convenience, you may want to have utils.py in your current working directory. 
+Initiating with `SEDSIMPLE` is as simple as:
+
+```python
+from utils import SEDSIMPLE
+from astrodbkit2.astrodb import Database
+
+db = Database('sqlite:///SIMPLE.db')
+sed = SEDSIMPLE(db, 'Trappist-1', auto_db=True)
+```
+
+The parameter `auto_db=True` tells it to load as much information from the provided database as it can. 
+This includes coordinates, parallaxes, spectral types, photometry, and spectra. 
+If you choose `auto_db=False` (the default), you can load whichever data you need with:
+ - sed.load_coords_db()
+ - sed.load_parallax_db()
+ - sed.load_photometry_db()
+ - sed.load_spectral_type_db()
+ - sed.load_spectra_db()
+
+Note: upon initialization `SEDSIMPLE` will search for the specified target in the database. 
+The internal database name may be different from what provided name. 
+You may want to manually check database entries with:
+
+```python
+db.search_object('Trappist-1')
+db.inventory('2MASS J23062928-0502285')  # primary source name in the database for Trappist-1
+```
+
+All methods available in `SED` are available in `SEDSIMPLE` which means you can proceed as normal.
+
+# TODO:
+ - Finish writing load_spectra_db
+ - Write example notebook
---
+++ utils.py
@@ -0,0 +1,125 @@
+# Wrapper to implement a subclass of SED that works natively with SIMPLE through Astrodbkit2
+# David R Rodriguez
+# 2022-08   Initial implementation
+
+import astropy.units as u
+from astropy.units.quantity import Quantity
+from astropy.coordinates import Angle, SkyCoord
+from sedkit import SED
+
+
+class SEDSIMPLE(SED):
+    def __init__(self, db, name, auto_db=False, **kwargs):
+        # ProcessClass initialization
+        super().__init__(name, **kwargs)
+
+        # Database variable
+        self.db = db
+
+        # Check if the name in the database is different
+        # If a match is found,  use that as a temporary overwrite of the name when fetching the inventory
+        Sources = db.search_object(name)
+        if len(Sources) == 1:
+            db_name = Sources['source'][0]
+            if db_name != name:
+                self.message(f'{name} matched to {db_name} in the database')
+                name = db_name
+
+        # Database inventory
+        self.inventory = db.inventory(name, pretty_print=False)
+
+        if auto_db:
+            self.load_coords_db()
+            self.load_parallax_db()
+            self.load_photometry_db()
+            self.load_spectral_type_db()
+            self.load_spectra_db()
+
+    def load_coords_db(self):
+        # Fetch coordiantes from database
+        table = 'Sources'
+        if table in self.inventory:
+            if len(self.inventory[table]) == 1:
+                data = self.inventory[table][0]
+                s = SkyCoord(ra=data['ra']*u.degree, dec=data['dec']*u.degree)
+                self.sky_coords = s
+            else:
+                self.message(f"More than one source in database: \n{self.inventory[table]}")
+        else:
+            self.message(f"Source {self.name} not in database.")
+
+    def load_parallax_db(self):
+        # Fetch parralax from database
+        table = 'Parallaxes'
+        if table in self.inventory:
+            # Fetch the adopted parallax
+            for row in self.inventory[table]:
+                if row['adopted']:
+                    value = Quantity(row['parallax'], unit=u.mas)
+                    error = Quantity(row['parallax_error'], unit=u.mas)
+
+                    # Fetch reference used in database
+                    bibcode = self.fetch_db_bibcode(row['reference'])
+
+                    # Store value
+                    self.parallax = value, error, bibcode
+        else:
+            self.message(f"No parallax for {self.name} in database.")
+
+    def load_photometry_db(self):
+        # Fetch photometry from database
+        table = 'Photometry'
+        if table in self.inventory:
+            # Fetch the adopted parallax
+            for row in self.inventory[table]:
+                bibcode = self.fetch_db_bibcode(row['reference'])
+                band = self._fix_bands(row['band'])
+                self.add_photometry(band, row['magnitude'], mag_unc=row['magnitude_error'], ref=bibcode)
+        else:
+            self.message(f"No photometry for {self.name} in database.")
+
+    def load_spectral_type_db(self):
+        # Fetch spectral type from database
+        table = 'SpectralTypes'
+        if table in self.inventory:
+            # Fetch the adopted spectral type
+            for row in self.inventory[table]:
+                if row['adopted']:
+                    bibcode = self.fetch_db_bibcode(row['reference'])
+                    self.spectral_type = row['spectral_type_string'], bibcode
+        else:
+            self.message(f"No spectral type for {self.name} in database.")
+
+    def load_spectra_db(self):
+        pass
+
+    def _fix_bands(self, band):
+        # Manual fixes to some SIMPLE bands
+        FIX_DICT = {'GAIA2': 'Gaia',
+                    'GAIA3': 'Gaia',
+                    'Gaia.Grp': 'Gaia.rp',
+                    'GaiaGbp': 'Gaia.bp'
+                    }
+        for key, value in FIX_DICT.items():
+            band = band.replace(key, value)
+        return band
+
+    def fetch_db_bibcode(self, ref):
+        # Utility function to fetch a reference from the database
+        try:
+            t = self.db.query(self.db.Publications.c.bibcode). \
+                filter(self.db.Publications.c.publication == ref). \
+                table()
+            bibcode = t['bibcode'][0]
+        except Exception as e:
+            self.message(f'Error trying to fetch references: {e}')
+            bibcode = None
+        return bibcode
+
+    def message(self, msg, pre='[SIMPLE]'):
+        # Overwrite message to use a custom prefix
+        if self.verbose:
+            if pre is None:
+                print(msg)
+            else:
+                print("{} {}".format(pre, msg))
